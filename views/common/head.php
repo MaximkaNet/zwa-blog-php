6b665189101d12fb6e043f7612d22215f508841b
Maksym <m.zavada2005@ukr.net>
2023-11-18 19:57:23 +0100

    Add link handlers

--- views/common/head.php
+++ views/common/head.php
@@ -2,7 +2,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/x-icon" href="/assets/images/favicon.ico">
+    <link rel="icon" type="image/x-icon" href="<?php echo Application::linkFor("/assets/images/favicon.ico"); ?>">
     <title><?php echo Application::getPageTitle(); ?></title>
-    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="stylesheet" href="<?php echo Application::linkFor("/assets/css/style.css")?>">
 </head>
