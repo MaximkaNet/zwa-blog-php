d02578635a657ad7db1b88039b38ad735d242a47
Maksym <m.zavada2005@ukr.net>
2023-11-18 19:54:36 +0100

    Add link generator, fixes

--- core/app.php
+++ core/app.php
@@ -5,12 +5,13 @@ namespace app\core;
 use app\core\router\Router;
 
 class Application {
-    private static $router;
-    private static $page_name;
-    private static $website_name;
-    private static $database;
+    private static Router $router;
+    private static string $page_name;
+    private static string $website_name;
+    private static string $home_dir = "/";
 
     /**
+     * Return the router object
      * @return Router
      */
     public static function getRouter(): Router
@@ -19,6 +20,7 @@ class Application {
     }
 
     /**
+     * Set router
      * @param Router $router
      */
     public static function setRouter(Router $router): void
@@ -27,6 +29,7 @@ class Application {
     }
 
     /**
+     * Return website name
      * @return string
      */
     public static function getWebsiteName(): string
@@ -35,6 +38,7 @@ class Application {
     }
 
     /**
+     * Set website name
      * @param string $website_name
      */
     public static function setWebsiteName(string $website_name): void
@@ -43,6 +47,7 @@ class Application {
     }
 
     /**
+     * Return page name
      * @return string
      */
     public static function getPageName(): string
@@ -51,6 +56,7 @@ class Application {
     }
 
     /**
+     * Set page name
      * @param string $page_name
      */
     public static function setPageName(string $page_name): void
@@ -63,13 +69,38 @@ class Application {
         return Application::getPageName() . ($with_website_name ? $separator . Application::getWebsiteName() : "");
     }
 
-    public static function getDatabase()
+    /**
+     * Return home directory
+     * @return mixed
+     */
+    public static function getHomeDir(): string
     {
-        return Application::$database;
+        return self::$home_dir;
     }
 
-    public static function setDatabase($database): void
+    /**
+     * Set home directory
+     * @param mixed $home_dir
+     */
+    public static function setHomeDir(string $home_dir): void
     {
-        Application::$database = $database;
+        self::$home_dir = $home_dir;
+    }
+
+    /**
+     * Generate a correct link
+     * @param string $url
+     * @return string
+     */
+    public static function linkFor(string $url): string
+    {
+        $path_with_prefix = self::$home_dir;
+        $path_with_prefix .= $url;
+
+        $parts = explode("/", $path_with_prefix);
+        $normalized = implode('/', array_filter($parts, function ($part) {
+            return $part != '';
+        }));
+        return '/' . $normalized;
     }
 }
--- services/users.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class UsersService {
-    public static function getById(int $id)
-    {
-
-    }
-}
