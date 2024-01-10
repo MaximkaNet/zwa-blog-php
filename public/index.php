09ae50c3581dc0546556627085203f43e66d0e59
Maksym <m.zavada2005@ukr.net>
2024-01-10 19:54:20 +0100

    Add dotenv handler and composer autoloader

--- public/index.php
+++ public/index.php
@@ -1,14 +1,19 @@
 <?php
 require_once "../vendor/autoloader.php";
+require_once "../vendor/autoload.php";
 require_once "../vendor/mustache/src/Mustache/Autoloader.php";
 
 Mustache_Autoloader::register();
 
 use app\core\Application;
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 $app = new Application();
 
-$router = $app->createRouter();
+$router = $app->createRouter($_ENV["URL_PREFIX"]);
 
 // Configure routes
 require "../config/routes/index.php";
