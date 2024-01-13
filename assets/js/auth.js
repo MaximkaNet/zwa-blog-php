afa9b0709bb762a591c78d29f30a06f9a0c9893b
Maksym <m.zavada2005@ukr.net>
2024-01-13 21:39:31 +0100

    Add validation

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,13 +1,14 @@
 import {PREFIX, API_URL_V1} from "./constants.js";
+import {Validator} from "./utils/validator/validator.js";
 
 const loginForm = document.getElementById("login_form");
 const signupForm = document.getElementById("signup_form");
 const logoutForm = document.getElementById("logout_form");
 
 function renderErrors(errors) {
-    if(!errors) return;
+    if (!errors) return;
     const errorContainer = document.querySelector(".auth-errors");
-    if(!errorContainer) {
+    if (!errorContainer) {
         console.error("Error container not found");
         return;
     }
@@ -32,32 +33,138 @@ function renderErrors(errors) {
     errors.map(appendError);
 }
 
-async function submitForm(event, uri, redirect = "/") {
-    event.preventDefault();
+/**
+ * Validate signup form and return form data
+ * @param {HTMLElement} form
+ * @return {FormData|null} Returns null if form is not valid
+ */
+function validateSignupForm(form) {
+    if (form == null) return null;
+    const formData = new FormData(form);
+    const first_name = formData.get("first_name");
+    const last_name = formData.get("last_name");
+    const email = formData.get("email");
+    const password = formData.get("password");
+    const password_confirm = formData.get("confirm_password");
+    const validation = {
+        first_name: Validator.name(first_name, "First name", true),
+        last_name: Validator.name(last_name, "Last name"),
+        email: Validator.email(email, true),
+        password: Validator.password(password, "Password", true),
+        password_confirm: Validator.password(password_confirm, "Confirm password", true),
+    };
+    let errors = [];
+    if(validation.first_name.isNotValid()) {
+        errors.push({message: validation.first_name.getMessage()})
+    }
+    if(validation.last_name.isNotValid()) {
+        errors.push({message: validation.last_name.getMessage()})
+    }
+    if(validation.email.isNotValid()) {
+        errors.push({message: validation.email.getMessage()})
+    }
+    if(validation.password.isNotValid()) {
+        errors.push({message: validation.password.getMessage()})
+    }
+    if(validation.password_confirm.isNotValid()) {
+        errors.push({message: validation.password_confirm.getMessage()})
+    }
+    if (errors.length === 0) {
+        if (password === password_confirm)
+            return formData;
+        else {
+            renderErrors([{message: "Passwords do not match"}]);
+            return null;
+        }
+    } else {
+        renderErrors(errors);
+        return null;
+    }
+}
+
+/**
+ * Validate login form and return form data
+ * @param {HTMLElement} form
+ * @return {FormData|null} Returns null if form is not valid
+ */
+function validateLoginForm(form) {
+    if (form == null) return null;
+    const formData = new FormData(form);
+    const email = formData.get("email");
+    const password = formData.get("password");
+    const email_validation = Validator.email(email, true);
+    let errors = [];
+    if (email_validation.isNotValid()) {
+        errors.push({message: email_validation.getMessage()})
+    }
+    if (password.trim() == "") {
+        errors.push({message: "Password cannot be empty"})
+    }
+    if (errors.length !== 0) {
+        renderErrors(errors);
+        return null;
+    }
+    return formData;
+}
+
+async function submitForm(data, uri) {
+    // Form validation
     const fetchOptions = {
         method: "POST",
-        body: new FormData(event.target)
+        body: data
     }
     const response = await fetch(API_URL_V1 + uri, fetchOptions);
-    const result = await response.json();
+    return await response.json();
+}
 
-    if(result.errors.length !== 0)
+async function submitSignupForm(event, uri, redirect = "/") {
+    event.preventDefault();
+    const validationResult = validateSignupForm(event.target);
+    if (validationResult === null) return;
+
+    const result = await submitForm(validationResult, uri)
+
+    if (result.errors.length !== 0)
         renderErrors(result.errors);
     else
         window.location.href = PREFIX + redirect;
 }
 
-if(loginForm) {
+async function submitLoginForm(event, uri, redirect = "/") {
+    event.preventDefault();
+    const validationResult = validateLoginForm(event.target);
+    if (validationResult === null) return;
+
+    const result = await submitForm(validationResult, uri)
+
+    if (result.errors.length !== 0)
+        renderErrors(result.errors);
+    else
+        window.location.href = PREFIX + redirect;
+}
+
+async function submitLogoutForm(event, uri, redirect = "/") {
+    event.preventDefault();
+
+    const result = await submitForm(event.target, uri)
+
+    if (result.errors.length !== 0)
+        renderErrors(result.errors);
+    else
+        window.location.href = PREFIX + redirect;
+}
+
+if (loginForm) {
     loginForm.addEventListener("submit", async (event) => {
-        await submitForm(event, "/login");
+        await submitLoginForm(event, "/login");
     });
 } else if (signupForm) {
     signupForm.addEventListener("submit", async (event) => {
-        await submitForm(event, "/signup", "/login");
+        await submitSignupForm(event, "/signup", "/login");
     });
 } else if (logoutForm) {
     logoutForm.addEventListener("submit", async (event) => {
-        await submitForm(event, "/logout", "/login");
+        await submitLogoutForm(event, "/logout", "/login");
     });
 } else {
     console.error("Suitable form is not found");
