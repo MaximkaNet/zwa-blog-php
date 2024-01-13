00cbd3ffe0fb9cbd894515e1d7f00246318d081a
Maksym <m.zavada2005@ukr.net>
2024-01-13 13:18:55 +0100

    Update Auth controller. Add validation and handle all exceptions

--- app/controllers/api/AuthAPIController.php
+++ app/controllers/api/AuthAPIController.php
@@ -4,6 +4,8 @@ namespace app\controllers\api;
 
 use app\core\exception\ApplicationException;
 use app\core\http\Response;
+use app\core\http\ServerRequest;
+use app\helpers\validator\Validator;
 use domain\users\UserException;
 use domain\users\UserService;
 
@@ -11,131 +13,125 @@ class AuthAPIController
 {
     /**
      * Login api
-     * @return string|null
+     * @return void
      */
-    public static function login(): ?array
+    public static function login(): void
     {
-        $response_body = new Response();
         header("Content-Type: application/json");
-        if (isset($_SESSION["user"])) {
-            $response_body->setErrors([
-                [
-                    "message" => "User already logged in"
-                ]
-            ]);
-            http_response_code(400);
-            echo $response_body->toJSON();
-            return null;
-        }
-
-        // Check input values
-        $validation_errors = [];
-        if (empty($_POST["email"])) {
-            $validation_errors[] = ["message" => "Email field is empty"];
-        }
-        if (empty($_POST["password"])) {
-            $validation_errors[] = ["message" => "Password field is empty"];
-        }
-
-        if (!empty($validation_errors)) {
-            $response_body->setErrors($validation_errors);
-            http_response_code(400);
-            echo $response_body->toJSON();
-            return null;
-        }
-
-        $service = new UserService();
-        // Process login
-        $email = htmlspecialchars($_POST["email"]);
-        $password = htmlspecialchars($_POST["password"]);
-
+        $response_body = new Response();
         try {
+            // Main logic
+            if (isset($_SESSION["user"])) {
+                throw new ApplicationException("User already logged in", 400);
+            }
+            $request_body = (new ServerRequest())->getParsedBody();
+
+            // Validate input data
+            $email_validation_result = Validator::email($request_body["email"] ?? null, true);
+            if ($email_validation_result->isNotValid()) {
+                throw new ApplicationException($email_validation_result->getMessage());
+            }
+            if (empty($request_body["password"])) {
+                throw new ApplicationException("Password is required");
+            }
+
+            $service = new UserService();
+            // Process login
+            $email = $request_body["email"];
+            $password = $request_body["password"];
             $user = $service->login($email, $password);
+            // Set session values
             $_SESSION["user"]["is_auth"] = true;
             $_SESSION["user"]["email"] = $user->getEmail();
             $_SESSION["user"]["id"] = $user->getId();
             $_SESSION["user"]["role"] = $user->getRole();
-            http_response_code(200);
+            // Success
             $response_body->setMessage("Login success");
-        } catch (UserException $exception) {
-            $response_body->setErrors([["message" => $exception->getMessage()]]);
-            $response_body->setData([
-                "user" => [
-                    "email" => $email
-                ]
-            ]);
-            http_response_code($exception->getCode());
+            $response_body->setResponseCode(200);
+        } catch (ApplicationException $e) {
+            $response_body->setResponseCode($e->getCode());
+            $response_body->addError($e->getMessage());
+        } catch (\Exception $e) {
+            $code = $e->getCode();
+            $response_body->addError($e->getMessage(), is_numeric($code) ? $code : null);
         }
+        http_response_code($response_body->getResponseCode());
         echo $response_body->toJSON();
-        return null;
     }
 
     /**
      * Signup api
-     * @return string|null
+     * @return void
      */
-    public static function signup(): ?array
+    public static function signup(): void
     {
         $response_body = new Response();
         header("Content-Type: application/json");
-
-        // Validation input fields
-        $validation_errors = [];
-        if (empty($_POST["first_name"])) {
-            $validation_errors[] = ["message" => "First name is required"];
-        }
-        if (empty($_POST["email"])) {
-            $validation_errors[] = ["message" => "Email is required"];
-        }
-        if (empty($_POST["password"])) {
-            $validation_errors[] = ["message" => "Password is required"];
-        }
-
-        if (!empty($validation_errors)) {
-            $response_body->setErrors($validation_errors);
-            http_response_code(400);
-            echo $response_body->toJSON();
-            return [];
-        }
-
-        $service = new UserService();
-        $first_name = htmlspecialchars($_POST["first_name"]);
-        $last_name = isset($_POST["last_name"]) ? htmlspecialchars($_POST["last_name"]) : null;
-        $email = htmlspecialchars($_POST["email"]);
-        $password = htmlspecialchars($_POST["password"]);
-
-        // Process registration
         try {
-            $user = $service->registration($email, $password, $first_name, $last_name);
-            http_response_code(201);
-            $response_body->setMessage("User is created");
-        } catch (UserException $exception) {
-            $response_body->setErrors([
-                ["message" => "User already exists"]
-            ]);
-            http_response_code(409);
+            $request_body = (new ServerRequest())->getParsedBody();
+            $validators = [
+                "first_name" => Validator::firstName($request_body["first_name"] ?? null, true),
+                "last_name" => Validator::lastName($request_body["last_name"] ?? null),
+                "email" => Validator::email($request_body["email"] ?? null, true),
+                "password" => Validator::password($request_body["password"] ?? null, true),
+                "confirm_password" => Validator::passwordConfirm($request_body["password_confirm"] ?? null, true)
+            ];
+
+            foreach ($validators as $validator) {
+                if ($validator->isNotValid()) {
+                    throw new ApplicationException($validator->getMessage(), 400);
+                }
+            }
+
+            // Check passwords
+            if ($request_body["password"] == $request_body["password_confirm"]) {
+                $service = new UserService();
+                $user = $service->registration(
+                    $request_body["email"],
+                    $request_body["password"],
+                    $request_body["first_name"],
+                    $request_body["last_name"] ?? null
+                );
+                $response_body->setResponseCode(201);
+                $response_body->setMessage("User is created");
+            } else {
+                throw new ApplicationException("Passwords do not match", 400);
+            }
+        } catch (ApplicationException $e) {
+            $response_body->setResponseCode($e->getCode());
+            $response_body->addError($e->getMessage());
+        } catch (\Exception $e) {
+            $code = $e->getCode();
+            $response_body->addError($e->getMessage(), is_numeric($code) ? $code : null);
         }
+        http_response_code($response_body->getResponseCode());
         echo $response_body->toJSON();
-        return [];
     }
 
     /**
      * Logout api
-     * @return string|null
+     * @return void
      */
-    public static function logout(): ?array
+    public static function logout(): void
     {
         $response = new Response();
         header("Content-Type: application/json");
-        if (isset($_SESSION["user"])) {
-            session_unset();
-            $response->setMessage("User logout successful");
-            http_response_code(200);
-        } else {
-            $response->setErrors([["message" => "User already logged out"]]);
-            http_response_code(400);
+        try {
+            if (isset($_SESSION["user"])) {
+                $_SESSION["user"] = null;
+                $response->setMessage("User logout successful");
+                $response->setResponseCode(200);
+            } else {
+                throw new ApplicationException("User already logged out", 400);
+            }
+        } catch (ApplicationException $e) {
+            $response->setResponseCode($e->getCode());
+            $response->addError($e->getMessage());
+        } catch (\Exception $e) {
+            $code = $e->getCode();
+            $response->addError($e->getMessage(), is_numeric($code) ? $code : null);
         }
+        http_response_code($response->getResponseCode());
         echo $response->toJSON();
-        return null;
     }
 }
